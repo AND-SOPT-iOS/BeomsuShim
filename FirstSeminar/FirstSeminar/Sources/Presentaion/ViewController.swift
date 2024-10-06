bd6ab28f458f15f1b527fde8e6356cd2cec63935
RAFA <rafa71802937@gmail.com>
2024-10-06 21:34:32 +0900

    feat: 각 요소 속성 설정 및 배치

--- FirstSeminar/FirstSeminar/Sources/Presentaion/ViewController.swift
+++ FirstSeminar/FirstSeminar/Sources/Presentaion/ViewController.swift
@@ -49,9 +49,51 @@ final class ViewController: BaseViewController {
         switchModeButton.do {
             $0.addTarget(self, action: #selector(switchModeButtonTapped), for: .valueChanged)
         }
+
+        titleTextField.do {
+            $0.configureTextInput(placeholder: "제목을 입력해주세요.", fontSize: 25, weight: .bold)
+        }
+
+        contentTextView.do {
+            $0.configureTextInput(placeholder: nil, fontSize: 17, weight: .medium)
+            $0.contentInset = .init(top: 10, left: 10, bottom: 10, right: 10)
+        }
+
+        nextButton.do {
+            $0.configureButton("다음", titleColor: .black, backgroundColor: .systemYellow)
+            $0.addTarget(self, action: #selector(navigateToDetailVC), for: .touchUpInside)
+        }
+
+        stackView.do {
+            $0.addArrangedSubviews(
+                titleTextField,
+                contentTextView
+            )
+            $0.configureStackView()
+        }
     }
 
     override func setUI() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: switchModeButton)
+        view.addSubviews(stackView, nextButton)
+        showPlaceholder()
+        configureToolbar()
+    }
+
+    override func setAutoLayout() {
+        titleTextField.snp.makeConstraints { $0.height.equalTo(80) }
+        contentTextView.snp.makeConstraints { $0.height.greaterThanOrEqualTo(10) }
+
+        nextButton.snp.makeConstraints {
+            $0.left.right.equalToSuperview().inset(16)
+            $0.bottom.equalTo(view.keyboardLayoutGuide.snp.top).offset(-5)
+            $0.height.equalTo(50)
+        }
+
+        stackView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
+            $0.left.right.equalToSuperview().inset(16)
+            $0.bottom.greaterThanOrEqualTo(nextButton.snp.top).offset(-10)
+        }
     }
 }
