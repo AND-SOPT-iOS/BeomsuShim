713e211147ceff08ca53b944c3fed08c241fc3dc
RAFA <rafa71802937@gmail.com>
2024-10-06 19:06:30 +0900

    feat: 바 버튼 아이템 추가 및 타이틀 변경 애니메이션 구현

--- FirstSeminar/FirstSeminar/Sources/Presentaion/ViewController.swift
+++ FirstSeminar/FirstSeminar/Sources/Presentaion/ViewController.swift
@@ -7,11 +7,51 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class ViewController: BaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK: - Properties
 
-        view.backgroundColor = .systemBackground
+    private let fadeTextAnimation = CATransition()
+    private let switchModeButton = UISwitch()
+    private let titleTextField = UITextField()
+    private let contentTextView = UITextView()
+    private let placeholderText = "내용을 입력해주세요."
+    private let nextButton = UIButton(type: .system)
+    private let stackView = UIStackView()
+
+    private var switchPushMode: Bool = false
+
+    // MARK: - Actions
+
+    @objc private func switchModeButtonTapped() {
+        switchPushMode.toggle()
+        applyFadeAnimationToNavigationBar()
+        updateNavigationTitle()
+    }
+
+    // MARK: - Helpers
+
+    private func updateNavigationTitle() {
+        navigationItem.title = switchPushMode ? "모달" : "내비게이션"
+    }
+
+    private func applyFadeAnimationToNavigationBar() {
+        fadeTextAnimation.duration = 0.3
+        fadeTextAnimation.type = .fade
+        navigationController?.navigationBar.layer.add(fadeTextAnimation, forKey: "fadeText")
+    }
+
+    // MARK: - UI
+
+    override func setStyle() {
+        navigationController?.navigationBar.topItem?.title = "내비게이션"
+
+        switchModeButton.do {
+            $0.addTarget(self, action: #selector(switchModeButtonTapped), for: .valueChanged)
+        }
+    }
+
+    override func setUI() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: switchModeButton)
     }
 }
