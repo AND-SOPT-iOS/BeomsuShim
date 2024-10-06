07ce85db7bc511e63b0ce3e1c99ae2942f2e837c
RAFA <rafa71802937@gmail.com>
2024-10-06 18:32:24 +0900

    init: 프로젝트 초기 세팅

---
+++ FirstSeminar/FirstSeminar/Sources/Application/SceneDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  SceneDelegate.swift
+//  FirstSeminar
+//
+//  Created by RAFA on 10/6/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let vc = ViewController()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: vc)
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
---
+++ FirstSeminar/FirstSeminar/Sources/Presentaion/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FirstSeminar
+//
+//  Created by RAFA on 10/6/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
