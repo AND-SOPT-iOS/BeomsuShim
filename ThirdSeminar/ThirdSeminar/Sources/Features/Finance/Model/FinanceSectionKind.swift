b7a315728f6e0106a16eef95a75293a9bea44f9e
RAFA <rafa71802937@gmail.com>
2024-10-30 02:12:45 +0900

    feat: 섹션 종류 정의

---
+++ ThirdSeminar/ThirdSeminar/Sources/Features/Finance/Model/FinanceSectionKind.swift
@@ -0,0 +1,12 @@
+//
+//  FinanceSectionKind.swift
+//  ThirdSeminar
+//
+//  Created by RAFA on 10/29/24.
+//
+
+import Foundation
+
+enum FinanceSectionKind: Int, CaseIterable {
+    case featured, essential, paidRanking, freeRanking
+}
