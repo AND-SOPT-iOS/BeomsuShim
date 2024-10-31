d686e9327c2b533875bd3165b6bb00b932611566
RAFA <rafa71802937@gmail.com>
2024-10-31 18:09:45 +0900

    refactor: 코드 개선

--- ThirdSeminar/ThirdSeminar/Sources/Features/Finance/Model/FinanceItem.swift
+++ ThirdSeminar/ThirdSeminar/Sources/Features/Finance/Model/FinanceItem.swift
@@ -7,17 +7,15 @@
 
 import UIKit
 
-protocol FinanceItem: Hashable {
-    var appIcon: String { get }
-    var title: String { get }
-    var subtitle: String { get }
-    var downloadState: DownloadState { get }
-    var price: Int? { get }
-}
+enum FeaturedCategory {
+    case new, recommended
 
-enum FinanceItemType: Hashable {
-    case featured(Featured)
-    case essential(Essential)
+    var categoryTitle: String {
+        switch self {
+        case .new: return "새로운 경험"
+        case .recommended: return "추천"
+        }
+    }
 }
 
 enum DownloadState {
@@ -29,73 +27,43 @@ enum DownloadState {
 
     var buttonTitle: String {
         switch self {
-        case .installed:
-            return "열기"
-        case .install:
-            return "받기"
-        case .reinstall:
-            return "icloud.and.arrow.down"
-        case .update:
-            return "업데이트"
-        case .paid:
-            return "₩"
+        case .installed: return "열기"
+        case .install: return "받기"
+        case .reinstall: return "icloud.and.arrow.down"
+        case .update: return "업데이트"
+        case .paid: return "₩"
         }
     }
 }
 
-struct Featured: FinanceItem {
+struct Item: Hashable {
     let id = UUID()
-    let category: String
+    let sectionType: FinanceSectionKind
+    let category: FeaturedCategory?
     let title: String
-    let description: String
+    let description: String?
     let appIcon: String
     let subtitle: String
     let downloadState: DownloadState
     let price: Int?
     let hasInAppPurchases: Bool
-    let backgroundColor: UIColor
-}
-
-struct Essential: FinanceItem {
-    let id = UUID()
-    let appIcon: String
-    let title: String
-    let subtitle: String
-    let downloadState: DownloadState
-    let price: Int?
-    let hasInAppPurchases: Bool
-}
-
-struct PaidRanking: FinanceItem {
-    let id = UUID()
-    let appIcon: String
-    let title: String
-    let subtitle: String
-    let downloadState: DownloadState
-    let price: Int?
-    let hasInAppPurchases: Bool
-}
-
-struct FreeRanking: FinanceItem {
-    let id = UUID()
-    let appIcon: String
-    let title: String
-    let subtitle: String
-    let downloadState: DownloadState
-    let price: Int?
-    let hasInAppPurchases: Bool
+    let backgroundColor: UIColor?
 }
 
 // MARK: - Mock Data
 
-extension Featured {
+extension Item {
 
-    static let featuredItems: [Featured] = [
+    static let items: [Item] = [
+
+        // MARK: - Featured
+
         .init(
-            category: "새로운 경험",
+            sectionType: .featured,
+            category: .new,
             title: "Fortune City - A Finance App",
             description: "게임을 하듯 관리하는 가계부",
-            appIcon: "hyundaicard",
+            appIcon: "1",
             subtitle: "좋은 소비습관을 만들어 보세요!",
             downloadState: .install,
             price: nil,
@@ -103,10 +71,11 @@ extension Featured {
             backgroundColor: .tertiarySystemGroupedBackground
         ),
         .init(
-            category: "추천",
+            sectionType: .featured,
+            category: .recommended,
             title: "Taxnote 부기 및 회계 앱",
             description: "간편한 장부 관리",
-            appIcon: "hyundaidepartment",
+            appIcon: "4",
             subtitle: "자영업자용 세금 환급을 위한 부기 및 회계 앱",
             downloadState: .install,
             price: nil,
@@ -114,10 +83,11 @@ extension Featured {
             backgroundColor: .systemIndigo
         ),
         .init(
-            category: "추천",
+            sectionType: .featured,
+            category: .recommended,
             title: "Currenzy",
             description: "빠르고 쉬운 환율 계산",
-            appIcon: "kakaobank",
+            appIcon: "2",
             subtitle: "금융",
             downloadState: .paid,
             price: 6_600,
@@ -125,93 +95,353 @@ extension Featured {
             backgroundColor: .systemPurple
         ),
         .init(
-            category: "새로운 경험",
+            sectionType: .featured,
+            category: .new,
             title: "Elk - 여행 환율 변환기",
             description: "언제 어디서나 간편한 환율 체크",
-            appIcon: "kakaopay",
+            appIcon: "0",
             subtitle: "Winner of ADA 2017",
             downloadState: .installed,
             price: nil,
             hasInAppPurchases: false,
             backgroundColor: .systemPink
-        )
-    ]
-}
+        ),
 
-extension Essential {
+        // MARK: - Essential
 
-    static let essentialItems: [Essential] = [
         .init(
-            appIcon: "kbank",
-            title: "Fin - 가계부 및 예산 추적기",
-            subtitle: "당신의 돈은 어디로 가고 있을까요?",
+            sectionType: .essential,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "0",
+            subtitle: "금융이 쉬워진다",
             downloadState: .install,
             price: nil,
-            hasInAppPurchases: Bool.random()
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
         ),
         .init(
-            appIcon: "kbpay",
-            title: "Buddy: Budget Planner App",
-            subtitle: "금융",
+            sectionType: .essential,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "1",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .installed,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .essential,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "2",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .reinstall,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .essential,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "3",
+            subtitle: "금융이 쉬워진다",
             downloadState: .update,
             price: nil,
-            hasInAppPurchases: Bool.random()
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
         ),
         .init(
-            appIcon: "mobileTmoney",
-            title: "YNAB",
-            subtitle: "금융",
-            downloadState: .reinstall,
-            price: nil,
-            hasInAppPurchases: Bool.random()
+            sectionType: .essential,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "4",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .paid,
+            price: 6_600,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
         ),
         .init(
-            appIcon: "monimo",
-            title: "Splitwise",
-            subtitle: "금융",
+            sectionType: .essential,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "5",
+            subtitle: "금융이 쉬워진다",
             downloadState: .install,
             price: nil,
-            hasInAppPurchases: Bool.random()
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
         ),
         .init(
-            appIcon: "kbank",
-            title: "Money Coach Budget Manager Pro",
-            subtitle: "Expense Tracker, Bill Planner",
+            sectionType: .essential,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "6",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .install,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .essential,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "7",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .install,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .essential,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "8",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .install,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+
+        // MARK: - Paid Ranking
+
+        .init(
+            sectionType: .paidRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "0",
+            subtitle: "금융이 쉬워진다",
             downloadState: .paid,
-            price: 990,
-            hasInAppPurchases: Bool.random()
+            price: 500,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
         ),
         .init(
-            appIcon: "pass",
-            title: "CardPointers for Credit Cards",
-            subtitle: "금융",
+            sectionType: .paidRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "11",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .paid,
+            price: 32_000,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .paidRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "10",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .paid,
+            price: 1_200,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .paidRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "9",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .paid,
+            price: 5_400,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .paidRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "8",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .paid,
+            price: 100,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .paidRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "7",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .paid,
+            price: 7_000,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .paidRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "6",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .paid,
+            price: 12_000,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .paidRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "5",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .paid,
+            price: 1_100,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .paidRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "4",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .paid,
+            price: 14_000,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+
+        // MARK: - Free Ranking
+
+        .init(
+            sectionType: .freeRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "0",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .install,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .freeRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "3",
+            subtitle: "금융이 쉬워진다",
             downloadState: .installed,
             price: nil,
-            hasInAppPurchases: Bool.random()
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
         ),
         .init(
-            appIcon: "kakaobank",
-            title: "iFinance 5",
-            subtitle: "금융",
-            downloadState: .paid,
-            price: 29_000,
-            hasInAppPurchases: Bool.random()
-        ),
-        .init(
-            appIcon: "toss",
-            title: "MoneyStats - Budget Planner",
-            subtitle: "금융",
-            downloadState: .installed,
+            sectionType: .freeRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "2",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .update,
             price: nil,
-            hasInAppPurchases: Bool.random()
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
         ),
         .init(
-            appIcon: "silson24",
-            title: "Pockey - 포켓 머니 매니저",
-            subtitle: "용돈 관리 등",
+            sectionType: .freeRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "1",
+            subtitle: "금융이 쉬워진다",
             downloadState: .reinstall,
             price: nil,
-            hasInAppPurchases: Bool.random()
-        )
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .freeRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "4",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .installed,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .freeRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "5",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .install,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .freeRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "6",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .install,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .freeRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "7",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .install,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
+        .init(
+            sectionType: .freeRanking,
+            category: nil,
+            title: "토스",
+            description: nil,
+            appIcon: "8",
+            subtitle: "금융이 쉬워진다",
+            downloadState: .install,
+            price: nil,
+            hasInAppPurchases: Bool.random(),
+            backgroundColor: nil
+        ),
     ]
+
+    static func items(for section: FinanceSectionKind) -> [Item] {
+        return items.filter { $0.sectionType == section }
+    }
 }
