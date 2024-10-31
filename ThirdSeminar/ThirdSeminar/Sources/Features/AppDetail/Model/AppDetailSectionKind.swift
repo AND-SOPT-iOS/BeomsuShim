aead399b07f69fed322b25ebec48bd580a76a97c
RAFA <rafa71802937@gmail.com>
2024-11-01 01:51:49 +0900

    feat: 앱 상세 페이지 섹션 정의

---
+++ ThirdSeminar/ThirdSeminar/Sources/Features/AppDetail/Model/AppDetailSectionKind.swift
@@ -0,0 +1,13 @@
+//
+//  AppDetailSectionKind.swift
+//  ThirdSeminar
+//
+//  Created by RAFA on 10/31/24.
+//
+
+import Foundation
+
+enum AppDetailSectionKind: Int, CaseIterable {
+    case header
+    case whatsNew
+}
