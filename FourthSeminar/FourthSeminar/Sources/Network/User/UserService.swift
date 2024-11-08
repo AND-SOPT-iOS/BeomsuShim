7209daae372282b232001e2ec7f667e0b677f45f
RAFA <rafa71802937@gmail.com>
2024-11-08 23:11:54 +0900

    feat: 다른 사람 취미 조회 로직 구현

--- FourthSeminar/FourthSeminar/Sources/Network/User/UserService.swift
+++ FourthSeminar/FourthSeminar/Sources/Network/User/UserService.swift
@@ -36,4 +36,55 @@ final class UserService {
         }
     }
 
+    static func fetchUserHobbyById(
+        userId: Int,
+        token: String,
+        completion: @escaping (Result<String, NetworkError>) -> Void
+    ) {
+        let url = "\(baseURL)/user/\(userId)/hobby"
+
+        AF.request(
+            url,
+            method: .get,
+            headers: ["token": token]
+        )
+        .validate()
+        .responseDecodable(of: MyHobbyResponseDTO.self) { response in
+            switch response.result {
+            case .success(let success):
+                let hobby = success.result.hobby
+                completion(.success(hobby))
+            case .failure:
+                completion(.failure(APIErrorManager.parseError(response: response)))
+            }
+        }
+    }
+
+    static func updateUserInfo(
+        hobby: String,
+        password: String,
+        token: String,
+        completion: @escaping (Result<Void, NetworkError>) -> Void
+    ) {
+        let url = "\(baseURL)/user"
+        let parameters: [String: Any] = ["hobby": hobby, "password": password]
+        let headers: HTTPHeaders = ["token": token]
+
+        AF.request(
+            url,
+            method: .put,
+            parameters: parameters,
+            encoding: JSONEncoding.default,
+            headers: headers
+        )
+        .validate()
+        .response { response in
+            switch response.result {
+            case .success:
+                completion(.success(()))
+            case .failure:
+                completion(.failure(APIErrorManager.parseError(response: response)))
+            }
+        }
+    }
 }
